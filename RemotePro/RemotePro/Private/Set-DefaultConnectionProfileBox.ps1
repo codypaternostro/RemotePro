4114dddee4a889248a50c531b8568c494b87925a
codypaternostro <cpaternostr@fingerlakes.edu>
2024-10-15 20:07:42 -0400

    Remove duplicate files.

--- RemotePro/RemotePro/Private/Set-DefaultConnectionProfileBox.ps1
+++
@@ -1,4 +0,0 @@
-# Function to set a default state when no profiles are available or an error occurs
-function Set-DefaultConnectionProfileBox {
-    $script:ConnectionProfileListBox.ItemsSource = @([pscustomobject]@{ Name = "No Profiles Found"; ServerAddress = ""; Credential = ""; FullDetails = "" })
-}
--- RemotePro/RemotePro/Private/Set-LoadingMessageTextBox.ps1
+++
@@ -1,5 +0,0 @@
-# Helper function for updating textbox
-function Set-LoadingMessageTextBox {
-    # Update connection status for text box
-    $script:Connection_Status_Box.Text = "Loading connection properties..."
-}
