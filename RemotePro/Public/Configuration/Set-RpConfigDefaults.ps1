ec2f42277ba405efcc3c3f14e78a7a2b336c7225
BabyKonanCoin <cpaternostr@fingerlakes.edu>
2025-01-01 20:11:29 -0500

    Updates to Set-RpConfigDefaults

--- RemotePro/Public/Configuration/Set-RpConfigDefaults.ps1
+++ RemotePro/Public/Configuration/Set-RpConfigDefaults.ps1
@@ -1,4 +1,30 @@
 function Set-RpConfigDefaults {
+    <#
+    .SYNOPSIS
+        Sets the default configuration for RemotePro.
+
+    .DESCRIPTION
+        This cmdlet sets the default configuration values for the RemotePro application. It ensures that all necessary settings are initialized with their default values.
+
+    .PARAMETER ConfigFilePath
+        The path to the configuration file where the default settings will be applied.
+
+    .EXAMPLE
+        PS C:\> Set-RpConfigDefaults -ConfigFilePath "C:\Config\RemoteProConfig.json"
+        This example sets the default configuration values in the specified configuration file.
+
+    .INPUTS
+        [string] The path to the configuration file where the default settings will be applied.
+
+    .OUTPUTS
+        None. This cmdlet does not produce any output.
+
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$ConfigFilePath
+    )
     # cmdlet to set the default configuration for RemotePro
 
     # ToDo: 11/28/24 add default configuration command entries with specific syntax
@@ -10,6 +36,7 @@ function Set-RpConfigDefaults {
     #                    -CommandNames "Get-RpEventHandlers", "Set-RpConfig" `
     #                    -Description "Initial configuration export"
 
+    #region add default commands
     $defaultCommands = [PSCustomObject]@{}
 
     $RemoteProCmds = ((Add-RpConfigCommand -ModuleName "RemotePro" -ConfigFilePath (Get-RpConfigPath) `
@@ -28,39 +55,90 @@ function Set-RpConfigDefaults {
     $defaultCommands | Add-Member -MemberType NoteProperty -Name "RemotePro" -Value $RemoteProCmds
     $defaultCommands | Add-Member -MemberType NoteProperty -Name "PSWriteHTML" -Value $PSWriteHTMLCmds
     $defaultCommands | Add-Member -MemberType NoteProperty -Name "MilestonePSTools" -Value $MilestonePSToolsCmds
+    #endregion
 
-    $params = @{
-        "CameraObject" = "`$platformItemcameras"
-        "SpecifiedDaysForSequences" = "7"
-        "DiagnosticLevel" = "3"
-    }
+    #region add runspace commands defaults to config file
+    # Explanation: The following command will be add to RemoteProParamConfig.json file.
+    # Stored in the RemotePro local appdata folder, and can be imported into a working object
+    # using the Import-RpConfig function. The module itself using the RemoteProController
+    # created using "$script:RemotePro = New-RpControllerObject" in the RemotePro.psm1 file.
 
-    $defaultCommands.remotepro | Where-Object -FilterScript {$_.commandname -like "Show-RpCamera"} |
-        Update-RpConfigCommand -Parameters $params -Verbose
-
-    #Out-HtmlView
+    #region Out-HtmlView
         #Out-HtmlView -EnableScroller -ScrollX -AlphabetSearch -SearchPane
+        $params = @{
+            "EnableScroller" = $true
+            "ScrollX" = $true
+            "AlphabetSearch" = $true
+            "SearchPane" = $true
+        }
 
-    #CamReport_Click
-        #Get-VmsCameraReport | Out-HtmlView -EnableScroller -ScrollX -AlphabetSearch -SearchPane
+        $defaultCommands.PSWriteHTML | Where-Object -FilterScript {$_.commandname -like "Out-HtmlView"} |
+            Update-RpConfigCommand -Parameters $params -Verbose
 
-    #ShowCameras_Click
+    #endregion
+
+    #region CamReport_Click
+        #Get-VmsCameraReport
+        $params = @{
+
+        }
+
+        $defaultCommands.MilestonePSTools | Where-Object -FilterScript {$_.commandname -like "Get-VmsCameraReport"} |
+            Update-RpConfigCommand -Parameters $params -Verbose
+    #endregion
+
+
+    #region ShowCameras_Click
         # command 1
-        #Show-RpCamera -CameraObject $platformItemcameras -SpecifiedDaysForSequences 7 -DiagnosticLevel 3 | Out-Null
+        #Show-RpCamera -CameraObject $platformItemcameras -SpecifiedDaysForSequences 7 -DiagnosticLevel 3
+        $params = @{
+            "CameraObject" = "`$platformItemcameras"
+            "SpecifiedDaysForSequences" = "7"
+            "DiagnosticLevel" = "3"
+        }
+
+        $defaultCommands.remotepro | Where-Object -FilterScript {$_.commandname -like "Show-RpCamera"} |
+            Update-RpConfigCommand -Parameters $params -Verbose
 
         # command 2
         #Get-RpTicketBlock -Cameras $configItemCams -ShowWindow
 
-    #TicketBlock_Click
+    #region TicketBlock_Click
         #Get-RpTicketBlock -Cameras $platformItemcameras -ShowWindow
+        $params = @{
+            "Cameras" = "`$configItemCams"
+            "ShowWindow" = $true
+        }
 
-    #ShowVideoOSItems_Click
+        $defaultCommands.remotepro | Where-Object -FilterScript {$_.commandname -like "Get-RpTicketBlock"} |
+            Update-RpConfigCommand -Parameters $params -Verbose
+    #endregion
+
+    #region ShowVideoOSItems_Click
         #Select-VideoOSItem
-        # might be some more entries to add here for modifiable commands
+        # Note: Might be some more entries to add here for modifiable commands
+        $defaultCommands.remotepro | Where-Object -FilterScript {$_.commandname -like "Select-VideoOSItem"} |
+            Update-RpConfigCommand -Verbose
+    #endregion
 
-    #Hardware_Click
+    #region Hardware_Click
         #Get-RpVmsHardwareCustom -CheckConnection
+        $params = @{
+            "CheckConnection" = $true
+        }
 
-    #ItemState_Click
+        $defaultCommands.remotepro | Where-Object -FilterScript {$_.commandname -like "Get-RpVmsHardwareCustom"} |
+            Update-RpConfigCommand -Parameters $params -Verbose
+    #endregion
+
+    #region ItemState_Click
         #Get-RpVmsItemStateCustom -CheckConnection
+        $params = @{
+            "CheckConnection" = $true
+        }
+
+        $defaultCommands.remotepro | Where-Object -FilterScript {$_.commandname -like "Get-RpVmsItemStateCustom"} |
+            Update-RpConfigCommand -Parameters $params -Verbose
+    #endregion
+    #endregion
 }
--- docs/en-US/Set-RpConfigDefaults.md
+++ docs/en-US/Set-RpConfigDefaults.md
@@ -13,7 +13,7 @@ schema: 2.0.0
 ## SYNTAX
 
 ```
-Set-RpConfigDefaults [<CommonParameters>]
+Set-RpConfigDefaults [-ConfigFilePath] <String> [-ProgressAction <ActionPreference>] [<CommonParameters>]
 ```
 
 ## DESCRIPTION
@@ -29,6 +29,36 @@ PS C:\> {{ Add example code here }}
 {{ Add example description here }}
 
 ## PARAMETERS
+
+### -ConfigFilePath
+The path to the configuration file where the default settings will be applied.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ProgressAction
+{{ Fill ProgressAction Description }}
+
+```yaml
+Type: ActionPreference
+Parameter Sets: (All)
+Aliases: proga
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
 
 ### CommonParameters
 This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
